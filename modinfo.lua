0301eb4032c7351bf21188bf6643149c42b8e5fe
Folk <vhaarr@gmail.com>
2017-12-07 17:14:20 +0100

    Set originalUploader, and bump to 1.9.x.

--- modinfo.lua
+++ modinfo.lua
@@ -4,8 +4,9 @@ return {
 	name = "Automated Behavior Ajdustment",
 	tags = { "Gameplay", "Total Conversion", "Spaceships", "Balance" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 799999383,
+	originalUploader = "folk",
 	readme = "readme.md",
 	steambb = "steam.bbcode",
 }
